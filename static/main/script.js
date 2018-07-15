94cff63a379505efd14182a7544c7b1c37d0762b
Simas2006 <sjuknelis@gmail.com>
2018-07-15 14:43:14 -0400

    Add check before updating

--- static/main/script.js
+++ static/main/script.js
@@ -1,3 +1,9 @@
 function quitApp() {
   console.log("TODO: send message to main");
 }
+
+function openUpdate() {
+  if ( confirm("Updating the server may take up to ten minutes. Are you sure you want to continue?") ) {
+    location.href = __dirname + "/../update/index.html";
+  }
+}
