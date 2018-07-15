4b930482b11aa593b29d9cf9b05d605a46a7c86d
Simas2006 <sjuknelis@gmail.com>
2018-07-14 21:04:26 -0400

    Add message about IP address

--- static/run/script.js
+++ static/run/script.js
@@ -1,17 +1,22 @@
 var fs = require("fs");
 var {exec} = require("child_process");
+var ip = require("ip");
 var proc;
 var playerList = [];
 var stopIntended = false;
 
+var port = 25565;
+var ramAlloc = 1;
+
 function startServer() {
+  document.getElementById("local-text").innerText = `Login locally using the IP address "${ip.address()}${port != 25565 ? ":" + port : ""}"`;
   document.getElementById("server-players").value = "[Players]\nNone";
   var logBox = document.getElementById("server-log");
   fs.readdir(`${__dirname}/../../server`,function(err,files) {
     if ( err ) throw err;
     files = files.filter(item => item.startsWith("spigot-") && item.endsWith(".jar"));
     if ( files.length <= 0 ) throw new Error("No usable JAR file");
-    proc = exec(`cd ${__dirname}/../../server && java -jar -Xms1G -Xmx1G ${files[0]}`,function(err,stdout,stderr) {
+    proc = exec(`cd ${__dirname}/../../server && java -jar -Xms${ramAlloc}G -Xmx${ramAlloc}G ${files[0]}`,function(err,stdout,stderr) {
       if ( err ) throw err;
     });
     proc.stdout.on("data",function(data) {
