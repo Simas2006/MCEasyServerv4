f62087c7bc6125fcaec340995531fdecc94c20cc
Simas2006 <sjuknelis@gmail.com>
2018-07-16 10:08:47 -0400

    Add openDirectory code

--- static/main/script.js
+++ static/main/script.js
@@ -1,9 +1,11 @@
-function quitApp() {
-  console.log("TODO: send message to main");
-}
+var {shell} = require("electron");
 
 function openUpdate() {
   if ( confirm("Updating the server may take up to ten minutes. Are you sure you want to continue?") ) {
     location.href = __dirname + "/../update/index.html";
   }
+}
+
+function openDirectory() {
+  shell.openItem(__dirname + "/../../server");
 }
